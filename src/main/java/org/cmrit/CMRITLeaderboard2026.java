4b569b2c80c8f149850c03af6dc0321b079fa08c
Gabriel <gabyah92@rocketmail.com>
2024-09-09 21:01:29 +0530

    Update CMRITLeaderboard2026.java

--- src/main/java/org/cmrit/CMRITLeaderboard2026.java
+++ src/main/java/org/cmrit/CMRITLeaderboard2026.java
@@ -866,7 +866,7 @@ public class CMRITLeaderboard2026 {
                 connection = websiteUrl.toURL().openConnection();
                 o = (HttpURLConnection) connection;
                 o.setRequestMethod("GET");
-                if(o.getResponseCode() == 500 ) { Thread.sleep(9000); continue; }
+                if(o.getResponseCode() == 500 ) { Thread.sleep(60000); continue; }
                 else if (o.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND || o.getResponseCode() == HttpURLConnection.HTTP_BAD_REQUEST) {
                     throw new RuntimeException();
                 }
@@ -880,7 +880,7 @@ public class CMRITLeaderboard2026 {
                     JSONObject jsonObject = new JSONObject(jsonContent.toString());
                     int codechefRating = 0;
                     try {
-                        Thread.sleep(9000);
+                        if( (i-2)%8 == 0 ) Thread.sleep(60000);
                         try {
                         codechefRating = jsonObject.getInt("currentRating"); 
                         } catch(Exception e) {
